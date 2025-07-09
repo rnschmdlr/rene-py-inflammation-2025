0a3650044c089f4c9dc8a745b43e8c3b80c192af
rnschmdlr <reneschmiedler@gmail.com>
2025-07-09 12:27:17 +0200

    Refactor test cases for daily mean and max functions; add CI workflow configuration

--- tests/test_models.py
+++ tests/test_models.py
@@ -6,51 +6,31 @@ import numpy.testing as npt
 
 from inflammation.models import *
 
-def test_daily_mean_zeros():
-    """Test that mean function works for an array of zeros."""
-    
+@pytest.mark.parametrize(
+    "test, expected",
+    [
+        ([ [0, 0], [0, 0], [0, 0] ], [0, 0]),
+        ([ [1, 2], [3, 4], [5, 6] ], [3, 4]),
+    ])
+def test_daily_mean(test, expected):
+    """Test mean function works for array of zeroes and positive integers."""
+    npt.assert_array_equal(daily_mean(np.array(test)), np.array(expected))
 
-    test_input = np.array([[0, 0],
-                           [0, 0],
-                           [0, 0]])
-    test_result = np.array([0, 0])
+@pytest.mark.parametrize(
+    "test, expected",
+    [
+        ([ [0, 0], [0, 0], [0, 0] ], [0, 0]),
+        ([ [1, 2], [3, 4], [5, 6] ], [5, 6]),
+        ([['Hello', 'there'], ['General', 'Kenobi']], 'raises')
+    ])
+def test_daily_max(test, expected):
+    """Test max function works for an array of zeroes, positive integers, and raises TypeError for strings."""
+    if expected == 'raises':
+        with pytest.raises(TypeError):
+            daily_max(test)
+    else:
+        npt.assert_array_equal(daily_max(np.array(test)), np.array(expected))
 
-    # Need to use Numpy testing functions to compare arrays
-    npt.assert_array_equal(daily_mean(test_input), test_result)
-
-
-def test_daily_mean_integers():
-    """Test that mean function works for an array of positive integers."""
-
-    test_input = np.array([[1, 2],
-                           [3, 4],
-                           [5, 6]])
-    test_result = np.array([3, 4])
-
-    # Need to use Numpy testing functions to compare arrays
-    npt.assert_array_equal(daily_mean(test_input), test_result)
-
-def test_daily_max_zeros():
-    """Test that max function works for an array of zeros."""
-
-    test_input = np.array([[0, 0],
-                           [0, 0],
-                           [0, 0]])
-    test_result = np.array([0, 0])
-
-    # Need to use Numpy testing functions to compare arrays
-    npt.assert_array_equal(daily_max(test_input), test_result)
-
-def test_daily_max_integers():
-    """Test that max function works for an array of positive integers."""
-
-    test_input = np.array([[1, 2],
-                           [3, 4],
-                           [5, 6]])
-    test_result = np.array([5, 6])
-
-    # Need to use Numpy testing functions to compare arrays
-    npt.assert_array_equal(daily_max(test_input), test_result)
 
 def test_daily_min_zeros():
     """Test that min function works for an array of zeros."""
