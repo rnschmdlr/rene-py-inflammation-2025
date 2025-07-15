4112b24d28f02500c12af224c284c92def70a791
rnschmdlr <reneschmiedler@gmail.com>
2025-07-15 10:11:29 +0200

    Enhance patient normalization function with input validation and NaN handling; update tests for improved coverage

--- inflammation/models.py
+++ inflammation/models.py
@@ -34,6 +34,23 @@ def daily_min(data):
 
 
 def patient_normalise(data):
-    """Normalise patient data from a 2D inflammation data array."""
-    max = np.max(data, axis=0)
-    return data / max[:, np.newaxis]
+    """
+    Normalise patient data between 0 and 1 of a 2D inflammation data array.
+
+    Any NaN values are ignored, and normalised to 0
+
+    :param data: 2D array of inflammation data
+    :type data: ndarray
+
+    """
+    if not isinstance(data, np.ndarray):
+        raise TypeError('data input should be ndarray')
+    if len(data.shape) != 2:
+        raise ValueError('inflammation array should be 2-dimensional')
+    if np.any(data < 0):
+        raise ValueError('inflammation values should be non-negative')
+    max_data = np.nanmax(data, axis=1)
+    with np.errstate(invalid='ignore', divide='ignore'):
+        normalised = data / max_data[:, np.newaxis]
+    normalised[np.isnan(normalised)] = 0
+    return normalised
--- tests/test_models.py
+++ tests/test_models.py
@@ -61,14 +61,34 @@ def test_daily_min_integers():
     npt.assert_array_equal(daily_min(test_input), test_result)
 
 @pytest.mark.parametrize(
-    "test, expected",
+    "test, expected, expect_raises",
     [
-        ([[1, 2, 3], [4, 5, 6], [7, 8, 9]], [[0.33, 0.67, 1], [0.67, 0.83, 1], [0.78, 0.89, 1]])
+        ...
+        (
+            'hello',
+            None,
+            TypeError,
+        ),
+        (
+            3,
+            None,
+            TypeError,
+        ),
+        (
+            [[1, 2, 3], [4, 5, 6], [7, 8, 9]],
+            [[0.33, 0.67, 1], [0.67, 0.83, 1], [0.78, 0.89, 1]],
+            None,
+        )
     ])
-def test_patient_normalise(test, expected):
-    """Test normalisation works for arrays of one and positive integers.
-       Test with a relative and absolute tolerance of 0.01."""
+def test_patient_normalise(test, expected, expect_raises):
+    """Test normalisation works for arrays of one and positive integers."""
+    if isinstance(test, list):
+        test = np.array(test)
+    if expect_raises is not None:
+        with pytest.raises(expect_raises):
+          result = patient_normalise(test)
+          npt.assert_allclose(result, np.array(expected), rtol=1e-2, atol=1e-2)
 
-    result = patient_normalise(np.array(test))
-    npt.assert_allclose(result, np.array(expected), rtol=1e-2, atol=1e-2)
-
+    else:
+        result = patient_normalise(test)
+        npt.assert_allclose(result, np.array(expected), rtol=1e-2, atol=1e-2)
